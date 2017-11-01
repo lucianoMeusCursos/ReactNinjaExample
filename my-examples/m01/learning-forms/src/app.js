79c9341472d17c16b60b961dbcc04a8de3b7ee1d
Luciano Baraúna <baraunaluciano@gmail.com>
2017-11-01 21:21:23 -0200

    iniciando estudos formulário

---
+++ my-examples/m01/learning-forms/src/app.js
@@ -0,0 +1,28 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      value: 'valor inicial'
+    }
+  }
+  render () {
+    return (
+      <div>
+       <form>
+         <input type="text" value={this.state.value} onChange={(e) => {
+           console.log(e)
+           this.setState({
+             value: e.target.value
+           })
+         }}/>
+       </form>
+      </div>
+    )
+  }
+}
+
+export default App
