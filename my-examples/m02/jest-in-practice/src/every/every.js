4bb5b7caa98f491ba008c7b0d9ee3bb6a0535641
Luciano Baraúna <baraunaluciano@gmail.com>
2017-12-01 20:23:05 -0200

    continuando m2a_17

--- my-examples/m02/jest-in-practice/src/every/every.js
+++ my-examples/m02/jest-in-practice/src/every/every.js
@@ -1,10 +1,11 @@
+'use strict'
+
 const every = (arr, func) => {
-    for(let i = 0; i < arr.lenght; i++) {
+    for(let i = 0; i < arr.length; i++) {
         if(!func(arr[i], i, arr)) {
             return false
         }
     }
-    
     return true
 }
 
