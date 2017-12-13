a34dcf5aa7e765c3a75903902cbc936849c94132
Luciano Baraúna <baraunaluciano@gmail.com>
2017-12-13 18:00:34 -0200

    finalizado m2a_26

--- my-examples/m02/jest-in-practice/src/reduce/reduce.test.js
+++ my-examples/m02/jest-in-practice/src/reduce/reduce.test.js
@@ -2,6 +2,7 @@
 
 import { expect } from 'chai'
 import reduce from './reduce'
+import reduceRight from './reduce-right'
 
 it('reduce should be a function', () => {
   expect(reduce).to.be.a('function')
@@ -43,5 +44,12 @@ it('reduce([1, 2], (acc, item, index) => acc + index, 0) should return 1', () =>
 it('reduce([1, 2], (acc, item, index, array) => acc + array[index], 0) should return 3', () => {
   const before = reduce([1, 2], (acc, item, index, array) => acc + array[index], 0)
   const after = 3
+  expect(before).to.be.equal(after)
+})
+
+
+it('reduceRight(["úna", "ra", "ba"], (acc, item) => acc + item, "") should return "baraúna"', () => {
+  const before = reduceRight(["úna", "ra", "ba"], (acc, item) => acc + item, '');
+  const after = 'baraúna'
   expect(before).to.be.equal(after)
 })
