f49a2b0f7ed19f9fd7e5b076e51453b3285c3116
Luciano Baraúna <baraunaluciano@gmail.com>
2017-12-27 17:48:32 -0200

    finalizado m2a_30

--- my-examples/m02/know-storybook/stories/index.js
+++ my-examples/m02/know-storybook/stories/index.js
@@ -3,6 +3,7 @@ import { storiesOf, action, linkTo } from '@kadira/storybook';
 import Button from './Button';
 import Welcome from './Welcome';
 
+
 storiesOf('Welcome', module)
   .add('to Storybook', () => (
     <Welcome showApp={linkTo('Button')}/>
