ff29436f2fd53c28efdd5e02a41159d63ae4ef92
Luciano Baraúna <baraunaluciano@gmail.com>
2018-01-12 19:21:22 -0200

    atualizando m2a_52

--- my-examples/m02/applications/github-app/src/utils/pagination/index.js
+++ my-examples/m02/applications/github-app/src/utils/pagination/index.js
@@ -1,24 +1,7 @@
 'use strict'
 
-// const pagination = (params) => { // sem destruction
-  // params.total
-  // params.activePage
-// return [1]
-// }
 
-// forma iperativa de retorno para total de páginas
 // const pagination = ({ total, activePage }) => {
-//   let pages = []
-//   for (let i = 0; i < total; i++){
-//     pages.push(i + 1)
-//   }
-//   return pages
-// }
-
-
-
-
-// const pagination = ({ total, activePage }) => { // usando destructin para descontruir o objeto es6
 
 //   return Array.apply(null, { length: total }).map((_, i) => i + 1) //HACK EM ES5 - array em js são objetos, quando passamos o objeto length dessa forma para um array ele vai entender que você vai criar um array com essa quantidade de objeto
 //   // aqui .map((_, i) => i + 1) estamos iterando pelos os elementos undefined que no caso é o _ no método map e estamos retornado como itens do array o item + 1 sendo assim seria o resultado [1, 2]
@@ -35,4 +18,3 @@ const pagination = ({ total, activePage }) => {
 
 
 export default pagination
-
--- my-examples/m02/applications/github-app/src/utils/pagination/pagination.test.js
+++ my-examples/m02/applications/github-app/src/utils/pagination/pagination.test.js
@@ -3,17 +3,17 @@
 import { expect } from 'chai'
 import pagination from './index'
 
-test('pagination should be a function', () => { // checando se é uma função
+test('pagination should be a function', () => {
   expect(pagination).to.be.a('function')
 })
 
-test('pagination({ total: 1, activePage: 1 }) should return [1]', () => { // checando o total de paginas
+test('pagination({ total: 1, activePage: 1 }) should return [1]', () => {
   const params = { total: 1, activePage: 1 }
   const result = [1]
   expect(pagination(params)).to.be.deep.equal(result)
 })
 
-test('pagination({ total: 2, activePage: 1 }) should return [1]', () => { // checando o total de paginas
+test('pagination({ total: 2, activePage: 1 }) should return [1]', () => {
   const params = { total: 2, activePage: 1 }
   const result = [1, 2]
   expect(pagination(params)).to.be.deep.equal(result)
