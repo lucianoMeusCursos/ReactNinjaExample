c441b48013821cbc995ae4a3df13f9617385346c
Luciano Baraúna <baraunaluciano@gmail.com>
2018-01-18 08:27:53 -0200

    finalizado m2a_53

--- my-examples/m02/applications/github-app/src/utils/pagination/pagination.test.js
+++ my-examples/m02/applications/github-app/src/utils/pagination/pagination.test.js
@@ -18,3 +18,15 @@ test('pagination({ total: 2, activePage: 1 }) should return [1]', () => {
   const result = [1, 2]
   expect(pagination(params)).to.be.deep.equal(result)
 })
+
+test('pagination({ total: 5, activePage: 1 }) should return [1, 2, 3, 4, 5]', () => { // testando se a quatindade for menor ou igual 5 ele vai mostrar todas as páginas
+  const params = { total: 5, activePage: 1 }
+  const result = [1, 2, 3, 4, 5]
+  expect(pagination(params)).to.be.deep.equal(result)
+})
+
+test('pagination({ total: 6, activePage: 1 }) should return [1, 2, 3, "...", 6]', () => {
+  const params = { total: 6, activePage: 1 }
+  const result = [1, 2, 3, "...", 6]
+  expect(pagination(params)).to.be.deep.equal(result)
+})
