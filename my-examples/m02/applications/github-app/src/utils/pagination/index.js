5283ebbdb0c74ed9aa3b72b364f1c7b39157aa21
Luciano Baraúna <baraunaluciano@gmail.com>
2018-01-19 18:20:16 -0200

    atualizando m2a_52

--- my-examples/m02/applications/github-app/src/utils/pagination/index.js
+++ my-examples/m02/applications/github-app/src/utils/pagination/index.js
@@ -1,20 +1,7 @@
 'use strict'
 
-
-// const pagination = ({ total, activePage }) => {
-
-//   return Array.apply(null, { length: total }).map((_, i) => i + 1) //HACK EM ES5 - array em js são objetos, quando passamos o objeto length dessa forma para um array ele vai entender que você vai criar um array com essa quantidade de objeto
-//   // aqui .map((_, i) => i + 1) estamos iterando pelos os elementos undefined que no caso é o _ no método map e estamos retornado como itens do array o item + 1 sendo assim seria o resultado [1, 2]
-// }
-
-
 const pagination = ({ total, activePage }) => {
-
-  return Array.from({ length: total }, (_, i) => i + 1) //ele pega os iterators ou os array like e cria itens de um array
-  // o primeiro parâmetro é o array like e o segundo é um método map já implementado que podemos iterar
+  return Array.from({ length: total }, (_, i) => i + 1)
 }
-
-
-
 
 export default pagination
