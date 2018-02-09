016d030279dbd8e6d063ea0af9fe68dfb1b86c5d
Luciano Baraúna <baraunaluciano@gmail.com>
2018-02-09 16:07:29 -0200

    finalizada m2a_70

--- my-examples/m02/applications/github-app/src/components/repos/index.js
+++ my-examples/m02/applications/github-app/src/components/repos/index.js
@@ -8,29 +8,32 @@ const Repos = ({ clasName, title, repos, handlePagination }) => (
   <div className={`repos-list-container ${clasName}`}>
     <h2>{title}</h2>
     <ul className='repos-list'>
-      {/* {repos.map((repo, index) => ( */}
       {repos.repos.map((repo, index) => (
         <li key={index}>
           <a href={repo.link}>{repo.name}</a>
         </li>
       ))}
     </ul>
-    <Pagination total={10} activePage={3} onClick={handlePagination} />
+    <Pagination total={10} activePage={repos.pagination.activePage} onClick={handlePagination} />
   </div>
 )
 
 Repos.defaultProps = {
-  className: '',
-  // repos: [] alteramos para objetos
-  repos: {}
+  className: ''
 }
 
 Repos.propTypes = {
   className: PropTypes.string,
   title: PropTypes.string.isRequired,
   handlePagination: PropTypes.func.isRequired,
-  // repos: PropTypes.array
-  repos: PropTypes.object
+  repos: PropTypes.arrayOf(PropTypes.shape({
+    link: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+  })).isRequired,
+  pagination: PropTypes.shape({
+    total: PropTypes.number,
+    activePage: PropTypes.number
+  }).isRequired
 }
 
 export default Repos
