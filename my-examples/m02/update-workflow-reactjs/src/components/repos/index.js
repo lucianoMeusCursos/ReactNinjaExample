43f210edf2318f5c280165c3348532b9b48b6b38
Luciano Baraúna <baraunaluciano@gmail.com>
2018-02-12 02:04:48 -0200

    finalizada m2a_72

---
+++ my-examples/m02/update-workflow-reactjs/src/components/repos/index.js
@@ -0,0 +1,43 @@
+'use strict'
+
+import React, { PropTypes } from 'react'
+import Pagination from 'components/pagination/'
+import './repos.css'
+
+const Repos = ({ clasName, title, repos, handlePagination }) => (
+  <div className={`repos-list-container ${clasName}`}>
+    <h2>{title}</h2>
+    <ul className='repos-list'>
+      {repos.repos.map((repo, index) => (
+        <li key={index}>
+          <a href={repo.link}>{repo.name}</a>
+        </li>
+      ))}
+    </ul>
+    <Pagination total={repos.pagination.total} activePage={repos.pagination.activePage} onClick={handlePagination} />
+  </div>
+)
+
+Repos.defaultProps = {
+  className: '',
+  activePage: 1
+}
+
+Repos.propTypes = {
+  className: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  handlePagination: PropTypes.func.isRequired,
+  repos: PropTypes.shape({
+    repos: PropTypes.arrayOf(PropTypes.shape({
+      link: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired
+    })).isRequired,
+  }).isRequired,
+  pagination: PropTypes.shape({
+    total: PropTypes.number,
+    activePage: PropTypes.number
+  })
+}
+
+export default Repos
+
---
+++ my-examples/m02/update-workflow-reactjs/storybook/webpack.config.js
@@ -0,0 +1,33 @@
+// you can use this file to add your custom webpack plugins, loaders and anything you like.
+// This is just the basic way to add addional webpack configurations.
+// For more information refer the docs: https://getstorybook.io/docs/configurations/custom-webpack-config
+
+// IMPORTANT
+// When you add this file, we won't add the default configurations which is similar
+// to "React Create App". This only has babel loader to load JavaScript.
+
+// module.exports = {
+//   plugins: [
+//     // your custom plugins
+//   ],
+//   module: {
+//     loaders: [
+//      {
+//       test: /\.css$/,
+//       exclude: /node_modules/,
+//       include: /src/,
+//       loaders: ['style', 'css']
+//     }
+//     ],
+//   },
+// };
+const path = require('path')
+const common = require('../webpack/common')
+const webpackConfig = require('@kadira/storybook/dist/server/config/defaults/webpack.config.js')
+
+module.exports = function (config, env) {
+  const newConfig = webpackConfig(config, env)
+  newConfig.module.preLoaders = (newConfig.module.preLoaders || []).concat(common.jsLoader)
+  newConfig.resolve = common.resolve
+  return newConfig
+}
