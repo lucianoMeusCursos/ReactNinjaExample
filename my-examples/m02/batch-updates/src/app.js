c50c3606e51fc2c18341be031b90ff9a7b4782c1
Luciano Baraúna <baraunaluciano@gmail.com>
2018-02-16 09:01:58 -0200

    finalizada m2a_80

--- my-examples/m02/batch-updates/src/app.js
+++ my-examples/m02/batch-updates/src/app.js
@@ -1,39 +1,74 @@
 'use strict'
 
 import React, { Component } from 'react'
+import Title from './components/title'
 
 import './css/style.css'
 
 class App extends Component {
   constructor () {
     super()
-    this.state = { counter: 0 }
+    this.state = { title: '...' }
   }
 
-  incrementCounter (state) {
-    return { counter: state.counter + 1 }
+  getTItle () {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('My app with async / await!')
+      }, 2000)
+    })
   }
 
-  incrementCounter2 (state) {
-    return { counter: state.counter + 2 }
-  }
-
-  componentDidMount () {
-    this.setState(this.incrementCounter)
-    this.setState(this.incrementCounter)
-    this.setState(this.incrementCounter)
-    this.setState(this.incrementCounter2)
+  async componentDidMount () {
+    let result
+    try {
+      this.setState({
+        title: await this.getTItle()
+      })
+    } catch (error) {}
   }
 
   render () {
-    console.log(`opa --- `, this.state)
-    const { counter } = this.state
-    console.log('render:', counter)
-
     return (
-      <div>counter: {counter}</div>
+      <div>
+        <Title>{this.state.title}</Title>
+      </div>
     )
   }
 }
+
+
+// class App extends Component {
+//   constructor () {
+//     super()
+//     this.state = { counter: 0 }
+//   }
+
+//   incrementCounter (state) {
+//     return { counter: state.counter + 1 }
+//   }
+
+//   incrementCounter2 (state) {
+//     return { counter: state.counter + 2 }
+//   }
+
+//   componentDidMount () {
+//     this.setState(this.incrementCounter)
+//     this.setState(this.incrementCounter)
+//     this.setState(this.incrementCounter)
+//     this.setState(this.incrementCounter2)
+//   }
+
+//   render () {
+//     console.log(`opa --- `, this.state)
+//     const { counter } = this.state
+//     console.log('render:', counter)
+
+//     return (
+//       <div>counter: {counter}</div>
+//     )
+//   }
+// }
+
 
 export default App
