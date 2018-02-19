0d3435ca7dd8d17a55150690a0e9aca8702eebb9
Luciano Baraúna <baraunaluciano@gmail.com>
2018-02-19 07:14:31 -0300

    finalizada m2a_86

--- my-examples/m02/batch-updates/src/app.js
+++ my-examples/m02/batch-updates/src/app.js
@@ -1,14 +1,17 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Title from './components/title'
+// import Title from './components/title'
 
 import './css/style.css'
 
 class App extends Component {
   constructor () {
     super()
-    this.state = { title: '...' }
+    this.state = {
+      title: '...',
+      Component: `div`
+    }
   }
 
   getTItle () {
@@ -20,18 +23,17 @@ class App extends Component {
   }
 
   async componentDidMount () {
-    let result
-    try {
-      this.setState({
-        title: await this.getTItle()
-      })
-    } catch (error) {}
+    const Title = await import(`components/title`)
+    this.setState({
+      title: await this.getTItle(),
+      Component: title.default
+    })
   }
 
   render () {
     return (
       <div>
-        <Title>{this.state.title}</Title>
+        <this.state.Component>{this.state.title}</this.state.Component>
       </div>
     )
   }
