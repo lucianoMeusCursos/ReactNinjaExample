b71c64cb5b989a6d7b62449867c4830390ece761
Luciano Baraúna <baraunaluciano@gmail.com>
2018-02-28 16:47:43 -0300

    iniciando m2a_94

---
+++ my-examples/m02/applications/markdown-editor/src/app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import React, { Component } from 'react'
+import './css/style.css'
+
+const App = () => (
+  <div className='editor'>
+    <textarea></textarea>
+    <div className='view'></div>
+  </div>
+)
+
+export default App
