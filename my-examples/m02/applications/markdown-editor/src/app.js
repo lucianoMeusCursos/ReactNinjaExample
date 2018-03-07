57e09f32092101e5abd732ee358769d693b7bee6
Luciano Baraúna <baraunaluciano@gmail.com>
2018-03-07 09:22:15 -0300

    finalizado m2a_94

--- my-examples/m02/applications/markdown-editor/src/app.js
+++ my-examples/m02/applications/markdown-editor/src/app.js
@@ -1,13 +1,37 @@
-'use strict'
-
 import React, { Component } from 'react'
 import './css/style.css'
 
-const App = () => (
-  <div className='editor'>
-    <textarea></textarea>
-    <div className='view'></div>
-  </div>
-)
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = { value: '' }
+    this.handleSubmit = (e) => {
+      e.preventDefault()
+      this.setState({
+        value: e.target.textarea.value
+      })
+    }
+
+  }
+
+
+  render() {
+    return (
+      <div className='editor'>
+        <form onSubmit={this.handleSubmit}>
+          <textarea name='textarea'></textarea>
+          <button>Renderizar markup</button>
+        </form>
+
+        <div className='view'>
+          {this.state.value}
+        </div>
+
+      </div>
+    )
+  }
+
+}
 
 export default App
