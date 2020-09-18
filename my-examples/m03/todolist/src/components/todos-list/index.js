4fc3f52755ed8f91ca70b14089f483d0bf3487ea
Luciano Baraúna <baraunaluciano@gmail.com>
2020-09-17 21:50:20 -0300

    finalizado m3#a47

--- my-examples/m03/todolist/src/components/todos-list/index.js
+++ my-examples/m03/todolist/src/components/todos-list/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleTodo } from './../../redux-flow/reducers/todos/actions-creators'
+import * as filterActions from './../../redux-flow/reducers/visibility-filter/actions'
 
-const TodosList = ({ todos, handleToggleTodo }) => (
+const TodosList = ({ todos, activeFilter, handleToggleTodo }) => (
   <ul>
-    {todos.map((todo) => (
+    {getVisibleTodos(todos, activeFilter).map((todo) => (
       <li
         key={todo.id}
         style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
@@ -16,9 +17,39 @@ const TodosList = ({ todos, handleToggleTodo }) => (
   </ul>
 )
 
+const getVisibleTodos = (todos, activeFilter) => {
+  // Refatorando o código.
+  // switch (activeFilter) {
+  //   case filterActions.SHOW_ALL:
+  //     return todos
+
+  //   case filterActions.SHOW_COMPLETED:
+  //     return todos.filter((todo) => todo.completed)
+
+  //   case filterActions.SHOW_ACTIVE:
+  //     return todos.filter((todo) => !todo.completed)
+
+  //   default:
+  //     return ''
+  // }
+  // const filterItems = {
+  //   [filterActions.SHOW_ALL]: todos,
+  //   [filterActions.SHOW_COMPLETED]: todos.filter((todo) => todo.completed),
+  //   [filterActions.SHOW_ACTIVE]: todos.filter((todo) => !todo.completed)
+  // }
+  // return filterItems[activeFilter]
+  return {
+    [filterActions.SHOW_ALL]: todos,
+    [filterActions.SHOW_COMPLETED]: todos.filter((todo) => todo.completed),
+    [filterActions.SHOW_ACTIVE]: todos.filter((todo) => !todo.completed)
+  }[activeFilter]
+
+}
+
 // Mapeando as props utilizadas
 const mapStateToProps = (state) => ({
-  todos: state.todos
+  todos: state.todos,
+  activeFilter: state.visibilityFilter
 })
 
 // Mapeando as actions utilizadas
