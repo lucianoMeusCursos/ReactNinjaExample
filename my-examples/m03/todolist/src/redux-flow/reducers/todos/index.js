04dd5f547916256c0ff064e9e05795f0c2c20580
Luciano Baraúna <baraunaluciano@gmail.com>
2020-09-18 20:00:52 -0300

    finalizada aula m3#a48

--- my-examples/m03/todolist/src/redux-flow/reducers/todos/index.js
+++ my-examples/m03/todolist/src/redux-flow/reducers/todos/index.js
@@ -1,30 +1,25 @@
+
 import { ADD_TODO, TOGGLE_TODO } from "./actions";
 
 export const initialState = [];
 
-const todos = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return state.concat({
-        id: action.payload.id,
-        text: action.payload.text,
-        completed: false
-      })
-    case TOGGLE_TODO:
-      return state.map((todo) => {
-        if (todo.id !== action.payload.id) {
-          return todo
-        }
+const todos = createReducer(initialState, {
+  [ADD_TODO]: (state, action) => state.concat({
+    id: action.payload.id,
+    text: action.payload.text,
+    completed: false
+  }),
 
-        return {
-          ...todo,
-          completed: !todo.completed
-        }
-      })
+  [TOGGLE_TODO]: (state, action) => state.map((todo) => {
+    if (todo.id !== action.payload.id) {
+      return todo
+    }
 
-    default:
-      return state
-  }
-}
+    return {
+      ...todo,
+      completed: !todo.completed
+    }
+  })
+})
 
 export default todos
