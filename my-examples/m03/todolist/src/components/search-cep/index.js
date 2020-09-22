8100a0d869682897a568d22cd61bed66f6daaff2
Luciano Baraúna <baraunaluciano@gmail.com>
2020-09-22 20:24:54 -0300

    finalizado m3#a55

--- my-examples/m03/todolist/src/components/search-cep/index.js
+++ my-examples/m03/todolist/src/components/search-cep/index.js
@@ -4,22 +4,28 @@ import ajax from '@fdaciuk/ajax';
 
 class SearchCepContaier extends PureComponent {
   state = {
-    address: "",
-    city: "",
-    code: "",
-    district: "",
+    address: '',
+    city: '',
+    code: '',
+    district: '',
     ok: true,
-    state: "",
+    state: '',
     status: 200,
-    statusText: "",
+    statusText: '',
+    message: '',
+    isFetching: false
   }
 
   handleSubmit = async (e) => {
     e.preventDefault()
+
+    this.setState({ isFetching: true })
+
     const cep = e.target.cep.value;
     const response = await ajax().get('https://ws.apicep.com/cep.json', { code: cep})
-    console.log(response);
 
+    this.setState({ isFetching: false })
+    console.log(response);
     this.setState(response)
   }
 
