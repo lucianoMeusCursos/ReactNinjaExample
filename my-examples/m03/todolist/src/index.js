e855ff872f352a287b1cf9da11356149c88b5c01
Luciano Baraúna <baraunaluciano@gmail.com>
2020-09-25 20:34:05 -0300

    finalizada m3#a63

--- my-examples/m03/todolist/src/index.js
+++ my-examples/m03/todolist/src/index.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import reducer from './redux-flow/reducers'
 import App from './App';
 import 'milligram';
 
-const initialState = {
-  todos: [{
-    id: 0,
-    text: 'Um exemplo com initialState',
-    completed: false
-  }]
+// Um exemplo para se criar um estado inicial na aplicação
+// const initialState = {
+//   todos: [{
+//     id: 0,
+//     text: 'Um exemplo com initialState',
+//     completed: false
+//   }]
+// }
+
+const middleware = ({dispatch, getState}) => (next) => (action) => {
+  console.log('LOGGER::will dispatch:', action)
+  const nextAction = next(action)
+  console.log('LOGGER::next action:', nextAction)
+  return nextAction
 }
+
+// const store = createStore(reducer, initialState)
+const store = createStore(reducer, applyMiddleware(middleware))
 
 const rederState = () => {
   console.log('state: ', store.getState())
 }
-
-const store = createStore(reducer, initialState)
 
 store.subscribe(rederState)
 rederState()
