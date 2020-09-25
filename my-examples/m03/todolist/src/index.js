322e6fc35c9338a46053cfd3f2285e10fe90a700
Luciano Baraúna <baraunaluciano@gmail.com>
2020-09-25 20:54:31 -0300

    finalziado m3#a64

--- my-examples/m03/todolist/src/index.js
+++ my-examples/m03/todolist/src/index.js
@@ -8,13 +8,13 @@ import App from './App';
 import 'milligram';
 
 // Um exemplo para se criar um estado inicial na aplicação
-// const initialState = {
-//   todos: [{
-//     id: 0,
-//     text: 'Um exemplo com initialState',
-//     completed: false
-//   }]
-// }
+const initialState = {
+  todos: [{
+    id: 0,
+    text: 'Um exemplo com initialState',
+    completed: false
+  }]
+}
 
 const middleware = ({dispatch, getState}) => (next) => (action) => {
   console.log('LOGGER::will dispatch:', action)
@@ -24,7 +24,7 @@ const middleware = ({dispatch, getState}) => (next) => (action) => {
 }
 
 // const store = createStore(reducer, initialState)
-const store = createStore(reducer, applyMiddleware(middleware))
+const store = createStore(reducer, initialState, applyMiddleware(middleware))
 
 const rederState = () => {
   console.log('state: ', store.getState())
