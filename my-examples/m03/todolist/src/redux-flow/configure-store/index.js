347b92d5db3a9ea9e3763a4e3aada1160e2dd9da
Luciano Baraúna <baraunaluciano@gmail.com>
2020-10-05 22:02:48 -0300

    finalizada aula m3#a78

--- my-examples/m03/todolist/src/redux-flow/configure-store/index.js
+++ my-examples/m03/todolist/src/redux-flow/configure-store/index.js
@@ -1,21 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from '../reducers'
 
-const logger = ({dispatch, getState}) => (next) => (action) => {
-  console.group(`LOGGER->${action.type}`)
-  console.log('will dispatch:', action)
-  console.log('state: ', getState())
-  const nextAction = next(action)
-  console.log('next state: ', getState())
-  console.groupEnd(`LOGGER->${action.type}`)
-  return nextAction
-}
-
 export default ({ initialState } = {}) => {
-  const store = createStore(rootReducer, initialState, applyMiddleware(logger, thunk))
+  const enhancer = compose(applyMiddleware(thunk), logger())
+  const store = createStore(rootReducer, initialState, enhancer)
   return store
 }
+
+const logger = () =>
+  window.__REDUX_DEVTOOLS_EXTENSION__
+  ? window.__REDUX_DEVTOOLS_EXTENSION__()
+  : (x) => x
+
 
 
 
