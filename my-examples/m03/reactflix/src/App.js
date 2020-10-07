0ce9a349226fb33e055f71e17eaa1d7223a9329a
Luciano Baraúna <baraunaluciano@gmail.com>
2020-10-07 20:56:16 -0300

    finalizada m3#a70, m3#a71

---
+++ my-examples/m03/reactflix/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import 'normalize.css';
+import 'milligram';
+
+
+const App = () => (
+  <div>
+    <header>
+      <h1>ReactFlix</h1>
+    </header>
+    <main>
+      conteúdo aqui
+    </main>
+    <footer>
+      Rodape copyright
+    </footer>
+  </div>
+)
+
+export default App;
