38802d0942200c1dd6684cf66dddfb52e6bda3f3
Luciano Baraúna <baraunaluciano@gmail.com>
2020-10-13 20:26:04 -0300

    finalizada m3#a72

--- my-examples/m03/reactflix/src/App.js
+++ my-examples/m03/reactflix/src/App.js
@@ -1,21 +1,48 @@
 import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 
 import 'normalize.css';
 import 'milligram';
 
 
+const GlobalStyle = createGlobalStyle`
+  html, body, #root, #root > div {
+    height: 100%;
+  }
+`
+
 const App = () => (
-  <div>
-    <header>
+  <Container>
+    <GlobalStyle />
+    <Header>
       <h1>ReactFlix</h1>
-    </header>
-    <main>
+    </Header>
+    <Main>
       conteúdo aqui
-    </main>
-    <footer>
+    </Main>
+    <Footer>
       Rodape copyright
-    </footer>
-  </div>
+    </Footer>
+  </Container>
 )
+
+const headerHeight = '60px';
+const footerHeight = '30px';
+
+const Container = styled.div``
+
+const Header = styled.header`
+  height: ${headerHeight};
+  background-color: #333;
+`
+
+const Main = styled.main`
+  min-height: calc(100% - ${headerHeight} - ${footerHeight});
+`
+
+const Footer = styled.footer`
+  height: ${footerHeight};
+  background: #333;
+`
 
 export default App;
