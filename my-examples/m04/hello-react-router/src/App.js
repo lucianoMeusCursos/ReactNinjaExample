b9069d8432599fea60e48bc4bfb541c7784487bb
Luciano Baraúna <baraunaluciano@gmail.com>
2021-03-17 19:12:52 -0300

    finalizada aula m4#a03

--- my-examples/m04/hello-react-router/src/App.js
+++ my-examples/m04/hello-react-router/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Link to="/sobre">Sobre</Link>
+      <Route path="/sobre" component={Sobre} />
+    </BrowserRouter>
   );
 }
+
+const Sobre = () => (
+  <h1>Sobre</h1>
+)
+
 
 export default App;
