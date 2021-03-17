6ecc8a4ed29082452d47999e544515f013376fa8
Luciano Baraúna <baraunaluciano@gmail.com>
2021-03-17 19:25:09 -0300

    finalizada m4#a04

--- my-examples/m04/hello-react-router/src/App.js
+++ my-examples/m04/hello-react-router/src/App.js
@@ -3,14 +3,30 @@ import { BrowserRouter, Link, Route } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
-      <Link to="/sobre">Sobre</Link>
+      <Route path="/" exact component={Home} />
       <Route path="/sobre" component={Sobre} />
+      <Route path="/contato" component={Contato} />
+
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/sobre">Sobre</Link></li>
+        <li><Link to="/contato">Contato</Link></li>
+      </ul>
+
     </BrowserRouter>
   );
 }
 
+const Home = () => (
+  <h1>Home</h1>
+)
+
 const Sobre = () => (
   <h1>Sobre</h1>
+)
+
+const Contato = () => (
+  <h1>Contato</h1>
 )
 
 
