7710ee9838ad7ae9ff58efe9f5c60ac218427df7
Luciano Baraúna <baraunaluciano@gmail.com>
2021-03-18 10:56:11 -0300

    Finalizada m4#a06

--- my-examples/m04/hello-react-router/src/App.js
+++ my-examples/m04/hello-react-router/src/App.js
@@ -3,15 +3,15 @@ import { BrowserRouter, Link, Route } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/sobre" component={Sobre} />
-      <Route path="/contato" component={Contato} />
-
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/sobre">Sobre</Link></li>
-        <li><Link to="/contato">Contato</Link></li>
+        <li><Link to="/blog">Blog</Link></li>
       </ul>
+
+      <Route path="/" exact component={Home} />
+      <Route path="/sobre" component={Sobre} />
+      <Route path="/blog" component={Blog} />
 
     </BrowserRouter>
   );
@@ -25,8 +25,24 @@ const Sobre = () => (
   <h1>Sobre</h1>
 )
 
-const Contato = () => (
-  <h1>Contato</h1>
+const Blog = () => (
+  <div>
+    <h1>Blog</h1>
+    <ul>
+      <li><Link to="/blog/post-1">Post 1</Link></li>
+      <li><Link to="/blog/post-2">Post 2</Link></li>
+    </ul>
+    <Route path="/blog/:post" component={Post} />
+    <Route exact to="/blog" component={NoPost} />
+  </div>
+)
+
+const NoPost = () => (
+  <h2>Selecione um post</h2>
+)
+
+const Post = ({ match }) => (
+  <h2>Post: {match.params.post}</h2>
 )
 
 
