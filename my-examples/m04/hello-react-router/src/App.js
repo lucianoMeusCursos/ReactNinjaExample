9811870b572502071c3ca0cf2cc784df8e418f08
Luciano Baraúna <baraunaluciano@gmail.com>
2021-03-25 11:21:28 -0300

    ajustando código

--- my-examples/m04/hello-react-router/src/App.js
+++ my-examples/m04/hello-react-router/src/App.js
@@ -2,81 +2,84 @@ import { BrowserRouter, NavLink, Route, Switch } from "react-router-dom";
 
 import './App.css'
 
-const LinkCustom = (props) => (
+const Link = (props) => (
   <NavLink activeStyle={{ color: 'red' }} {...props} />
 )
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <ul>
-          <li><LinkCustom to="/" exact>Home</LinkCustom></li>
-          <li><LinkCustom to="/sobre">Sobre</LinkCustom></li>
-          <li><LinkCustom to="/contato">Contato</LinkCustom></li>
-          <li><LinkCustom to="/blog">Blog</LinkCustom></li>
-        </ul>
+        <div>
+          <ul>
+            <li><Link to='/' exact>Home</Link></li>
+            <li><Link to='/sobre'>Sobre</Link></li>
+            <li><Link to='/contato'>Contato</Link></li>
+            <li><Link to='/blog'>Blog</Link></li>
+          </ul>
 
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/(sobre|contato)" component={Page} />
-          <Route path="/blog" component={Blog} />
-          <Route component={Error404} />
-        </Switch>
-
-        <Route path='/sobre'>
-          {({ match }) => (
-            <pre>
-              {console.log('Estou na página sobre', match)}
-              Estou na página sobre ! {`${!!match}`}
-            </pre>
-          )}
-        </Route>
-      </div>
-    </BrowserRouter>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/(sobre|contato)' component={Page} />
+            <Route path='/blog' component={Blog} />
+            <Route component={Error404} />
+          </Switch>
+        </div>
+      </BrowserRouter>
   );
 }
-
 const Error404 = () => (
-  <h1>Página não encotrada</h1>
+  <h1>Página não encontrada</h1>
 )
 
-const Home = () => (
-  <h1>Home</h1>
+const Home = ({ match }) => (
+  <div>
+    {console.log('Home match:', match)}
+    <h1>Home</h1>
+  </div>
 )
 
 const Page = ({ match }) => (
-  <h1>{match.url}</h1>
+  <div>
+    {console.log('Page match:', match)}
+    <h1>{match.url}</h1>
+  </div>
 )
 
-const Blog = () => (
+let blogMatch = null
+const Blog = ({ match }) => (
   <div>
+    {console.log('Blog match:', blogMatch = match)}
     <h1>Blog</h1>
+
     <ul>
-      <li><LinkCustom to="/blog/post-1">Post 1</LinkCustom></li>
-      <li><LinkCustom to="/blog/post-2">Post 2</LinkCustom></li>
+      <li><Link to='/blog/post-1'>Post 1</Link></li>
+      <li><Link to='/blog/post-2'>Post 2</Link></li>
     </ul>
 
     <Switch>
-      <Route exact to="/blog" render={ () => <NoPosts numberOfPost={2}/>} />
+      <Route exact path='/blog' render={(props) => <NoPosts numberOfPosts={2} {...props} />} />
       <Route path='/blog/:post(post-[12])' component={Post} />
       <Route component={Post404} />
     </Switch>
   </div>
 )
 
-const Post404 = () => (
-  <h1>Esse post não existe</h1>
+const Post404 = ({ match }) => (
+  <div>
+    {console.log('Post404 match:', match, 'Post404 match é o mesmo do blog?', match === blogMatch)}
+    <h1>Esse post não existe</h1>
+  </div>
 )
 
-const Post = ({match}) => (
+const Post = ({ match }) => (
   <div>
+    {console.log('Post match:', match)}
     <h2>Post: {match.params.post}</h2>
   </div>
 )
 
-const NoPosts = ({ numberOfPost }) => (
-  <h2>Selecione um dos {numberOfPost} post</h2>
+const NoPosts = ({ numberOfPosts }) => (
+  <p>Selecione um dos {numberOfPosts} posts</p>
 )
 
 
