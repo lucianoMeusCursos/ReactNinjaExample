78fda2cb64838771fc0144512e3a2df04fe29ee1
Luciano Baraúna <baraunaluciano@gmail.com>
2021-03-29 12:18:34 -0300

    finalizada aula m4#a30

--- my-examples/m04/hello-react-router/src/App.js
+++ my-examples/m04/hello-react-router/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, NavLink, Route, Switch } from "react-router-dom";
+import { BrowserRouter, NavLink, Route, Switch, Prompt } from "react-router-dom";
 
 import './App.css'
 
@@ -6,14 +6,31 @@ const Link = (props) => (
   <NavLink activeStyle={{ color: 'red' }} {...props} />
 )
 
+const ButtonBack = ({ history }) => (
+  <button onClick={(e) => history.goBack()}>Voltar</button>
+)
+
+const ButtonForward = ({ history }) => (
+  <button onClick={(e) => history.goForward()}>Próxima página</button>
+)
+
 function App() {
   return (
     <BrowserRouter>
         <div>
           <ul>
+            <li>
+              <Route component={ButtonBack} />
+            </li>
+            <li>
+              <Route component={ButtonForward} />
+            </li>
+          </ul>
+          <ul>
             <li><Link to='/' exact>Home</Link></li>
             <li><Link to='/sobre'>Sobre</Link></li>
             <li><Link to='/contato'>Contato</Link></li>
+            <li><Link to='/cadastro'>Cadastro</Link></li>
             <li><Link to='/blog'>Blog</Link></li>
           </ul>
 
@@ -21,34 +38,41 @@ function App() {
             <Route path='/' exact component={Home} />
             <Route path='/(sobre|contato)' component={Page} />
             <Route path='/blog' component={Blog} />
+            <Route path='/cadastro' component={Register} />
             <Route component={Error404} />
           </Switch>
         </div>
       </BrowserRouter>
   );
 }
+
+const Register = () => (
+  <Prompt when={true} message='Navegação bloqueada!' />
+)
 const Error404 = () => (
   <h1>Página não encontrada</h1>
 )
 
-const Home = ({ match }) => (
+const Home = ({ match, location }) => (
   <div>
     {console.log('Home match:', match)}
+    {console.log('Home location:', location)}
     <h1>Home</h1>
   </div>
 )
 
-const Page = ({ match }) => (
+const Page = ({ match, location }) => (
   <div>
-    {console.log('Page match:', match)}
+    {console.log('Page location:', location)}
     <h1>{match.url}</h1>
   </div>
 )
 
 let blogMatch = null
-const Blog = ({ match }) => (
+const Blog = ({ match, location }) => (
   <div>
-    {console.log('Blog match:', blogMatch = match)}
+    {console.log('Blog match:', match)}
+    {console.log('Blog location:', location)}
     <h1>Blog</h1>
 
     <ul>
@@ -64,16 +88,17 @@ const Blog = ({ match }) => (
   </div>
 )
 
-const Post404 = ({ match }) => (
+const Post404 = ({ match, location }) => (
   <div>
     {console.log('Post404 match:', match, 'Post404 match é o mesmo do blog?', match === blogMatch)}
+    {console.log('Location post', location)}
     <h1>Esse post não existe</h1>
   </div>
 )
 
-const Post = ({ match }) => (
+const Post = ({ match, location }) => (
   <div>
-    {console.log('Post match:', match)}
+    {console.log('Post location:', location)}
     <h2>Post: {match.params.post}</h2>
   </div>
 )
