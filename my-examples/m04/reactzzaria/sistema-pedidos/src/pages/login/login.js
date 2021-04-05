9981de1d353468cbed02ad6a62f54de39535cded
Luciano Baraúna <baraunaluciano@gmail.com>
2021-04-05 11:57:27 -0300

    finalizado m4#a50

--- my-examples/m04/reactzzaria/sistema-pedidos/src/pages/login/login.js
+++ my-examples/m04/reactzzaria/sistema-pedidos/src/pages/login/login.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
-import { Button } from '@material-ui/core'
+import { Button, Grid } from '@material-ui/core'
 import { ReactComponent as Logo } from './logo-react-zzaria.svg'
 
 const Login = () => (
   <>
-    <Logo />
-    <Button>Entra com Github</Button>
+    <Grid container>
+      <Grid item xs={12}>
+        <Logo />
+      </Grid>
+      <Grid item xs={12}>
+        <Button>Entra com Github</Button>
+      </Grid>
+    </Grid>
   </>
 )
 
