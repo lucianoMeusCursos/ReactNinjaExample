a0432982f7b95c6df487f5ffc55fce4619b05839
Luciano Baraúna <baraunaluciano@gmail.com>
2021-04-05 12:23:59 -0300

    finalizada m4#a52

--- my-examples/m04/reactzzaria/sistema-pedidos/src/pages/login/login.js
+++ my-examples/m04/reactzzaria/sistema-pedidos/src/pages/login/login.js
@@ -2,20 +2,26 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { Button, Grid } from '@material-ui/core'
-import { ReactComponent as Logo } from './logo-react-zzaria.svg'
+import { ReactComponent as MainLogo } from './logo-react-zzaria.svg'
 
 const Login = () => (
   <Container>
     <Grid
       container
       direction='column'
-      alignItems='center'
+      justify='center'
     >
       <Grid item xs={12}>
         <Logo />
       </Grid>
+
       <Grid item xs={12}>
-        <Button>Entra com Github</Button>
+        <GithubButton
+          variant='contained'
+          fullWidth
+        >
+          Entra com Github
+        </GithubButton>
       </Grid>
     </Grid>
   </Container>
@@ -23,6 +29,18 @@ const Login = () => (
 
 const Container = styled.div`
   padding: 20px;
+`
+
+const Logo = styled(MainLogo)`
+  width: 100%;
+`
+
+const GithubButton = styled(Button)`
+  && {
+    font-size: 20px;
+    padding: 15px;
+    text-transform: none;
+  }
 `
 
 export default Login
