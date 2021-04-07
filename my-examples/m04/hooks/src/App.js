448d0019df39adfe06a16db8aad3321f18eb58fd
Luciano Baraúna <baraunaluciano@gmail.com>
2021-04-07 09:10:35 -0300

    finalizada m4#a63

--- my-examples/m04/hooks/src/App.js
+++ my-examples/m04/hooks/src/App.js
@@ -14,32 +14,50 @@ class CounterClass extends React.PureComponent {
 
   render () {
     return (
-    <Counter counter={this.state.counter} increment={() => {
-      this.setState((prevState) => ({
-        counter: prevState.counter + 1
-      }))
-    }}
-    decrement={() => {
-      this.setState((prevState) => ({
-        counter: prevState.counter - 1
-      }))
-    }} />
+    <>
+      <Counter counter={this.state.counter} increment={() => {
+        this.setState((prevState) => ({
+          counter: prevState.counter + 1
+        }))
+      }}
+      decrement={() => {
+        this.setState((prevState) => ({
+          counter: prevState.counter - 1
+        }))
+      }} />
+      {this.state.toggle && <h1>Visível!</h1>}
+      <button onClick={() => {
+        this.setState((prevState) => ({
+          toggle: !prevState.toggle
+        }))
+      }}>Toggle</button>
+    </>
     )
   }
 }
 
 function CounterFunction () {
   const [counter, setCounter] = useState(0)
-  console.log('counter', counter)
+  const [toggle, setToggle] = useState(false)
 
-  return ( <Counter
-    counter={counter}
-    increment={() => {
-      setCounter(counter => counter + 1)
-    }}
-    decrement={() => {
-      setCounter(counter => counter - 1)
-    }} /> )
+
+  return (
+    <>
+      <Counter
+        counter={counter}
+        increment={() => {
+          setCounter(counter => counter + 1)
+        }}
+        decrement={() => {
+          setCounter(counter => counter - 1)
+        }}
+      />
+      {toggle && <h1>Visível!</h1>}
+      <button onClick={() => {
+        setToggle(!toggle)
+      }}>Toggle</button>
+    </>
+  )
 }
 
 const Counter = ({counter, increment, decrement}) => (
