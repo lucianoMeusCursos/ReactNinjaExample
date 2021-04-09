8fc515944d1e1fad56ab0a7a3e20c70a5b49b277
Luciano Baraúna <baraunaluciano@gmail.com>
2021-04-09 12:45:05 -0300

    finalizada aula m4#a73

--- my-examples/m04/reactzzaria/sistema-pedidos/src/app.js
+++ my-examples/m04/reactzzaria/sistema-pedidos/src/app.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { CssBaseline } from '@material-ui/core'
+import { CssBaseline, LinearProgress } from '@material-ui/core'
 
-import { MainPage } from 'pages/main'
-import { Login } from 'pages/login'
+// Fazendo o import dinâmico.
+const MainPage = lazy(() => import('pages/main'))
+const Login = lazy(() => import('pages/login'))
 
 const App = () => (
   <>
     <CssBaseline />
     <BrowserRouter>
-      <Switch>
-        <Route path='/login' component={Login} />
-        <Route component={MainPage} />
-      </Switch>
+      <Suspense fallback={<LinearProgress />}>
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route component={MainPage} />
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   </>
 )
