1304fc536bce01512a1401f89934626fd623bd4e
Luciano Baraúna <baraunaluciano@gmail.com>
2021-04-13 09:29:14 -0300

    finalizada aula m4#a78

--- my-examples/m04/reactzzaria/sistema-pedidos/src/app.js
+++ my-examples/m04/reactzzaria/sistema-pedidos/src/app.js
@@ -1,12 +1,28 @@
-import React, { lazy, Suspense } from 'react'
+import React, { lazy, Suspense, useEffect, useContext } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { LinearProgress } from '@material-ui/core'
+
+import firebase from 'services/firebase'
+
+import { AuthContext } from 'contexts/auth'
 
 // Fazendo o import dinâmico.
 const MainPage = lazy(() => import('pages/main'))
 const Login = lazy(() => import('pages/login'))
 
 function App () {
+  const { setUserInfo } = useContext(AuthContext)
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log('dados do usuário', user)
+      setUserInfo({
+        isUserLoggedIn: !!user,
+        user
+      })
+    })
+  }, [])
+
   return (
     // Suspense faz parte do import dinâmico
     // é nele que precisamos colocar como filho
--- my-examples/m04/reactzzaria/sistema-pedidos/src/pages/login/login.js
+++ my-examples/m04/reactzzaria/sistema-pedidos/src/pages/login/login.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, useCallback, useContext } from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
-import firebase from 'services/firebase'
 
 import { AuthContext } from 'contexts/auth'
 
@@ -8,34 +7,7 @@ import { Button, Grid } from '@material-ui/core'
 import { ReactComponent as MainLogo } from './logo-react-zzaria.svg'
 
 function Login () {
-  const { handleLogin } = useContext(AuthContext)
-
-  const [userInfo, setUserInfo] = useState({
-    isUserLoggedIn: false,
-    user: null
-  })
-
-  const { isUserLoggedIn, user } = userInfo
-
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
-      console.log('dados do usuário', user)
-      setUserInfo({
-        isUserLoggedIn: !!user,
-        user
-      })
-    })
-  }, [])
-
-  const handleLogout = useCallback(() => {
-    firebase.auth().signOut().then(() => {
-      console.log('Deslogou')
-      setUserInfo({
-        isUserLoggedIn: false,
-        user: null
-      })
-    })
-  }, [])
+  const { login } = useContext(AuthContext)
 
   return (
     <Container>
@@ -49,18 +21,9 @@ function Login () {
         </Grid>
 
         <Grid item xs={12} container justify='center'>
-          {isUserLoggedIn && (
-            <>
-              <pre>{user.displayName}</pre>
-              <Button variant='contained' onClick={handleLogout}>Sair</Button>
-            </>
-          )}
-
-          {!isUserLoggedIn && (
-            <GithubButton onClick={handleLogin}>
-              Entra com Github
-            </GithubButton>
-          )}
+          <GithubButton onClick={login}>
+            Entra com Github
+          </GithubButton>
         </Grid>
       </Grid>
     </Container>
