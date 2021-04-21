100eb7126f6f5bb2ee3df1fe8eda04522bdafde9
Luciano Baraúna <baraunaluciano@gmail.com>
2021-04-21 17:37:11 -0300

    finalizada m4#a89

--- my-examples/m04/reactzzaria/sistema-pedidos/src/pages/main/main.js
+++ my-examples/m04/reactzzaria/sistema-pedidos/src/pages/main/main.js
@@ -1,12 +1,15 @@
 import React, { useContext, useState } from 'react'
+import t from 'prop-types'
 import styled from 'styled-components'
 import {
   AppBar,
-  Toolbar as MaterialToolbar,
+  Grid,
   IconButton,
-  Typography,
   Menu,
-  MenuItem
+  MenuItem,
+  Toolbar as MaterialToolbar,
+  Typography,
+  withStyles
 } from '@material-ui/core'
 import { AccountCircle } from '@material-ui/icons'
 import { ReactComponent as MainLogo } from 'images/logo-react-zzaria.svg'
@@ -16,6 +19,7 @@ import { AuthContext } from 'contexts/auth'
 const Main = () => {
   const [anchorElement, setAnchorElement] = useState(null)
   const { userInfo, logout } = useContext(AuthContext)
+  const userName = userInfo.user.displayName.split(' ')[0]
 
   const handleOpenMenu = (e) => {
     setAnchorElement(e.target)
@@ -26,25 +30,40 @@ const Main = () => {
   }
 
   return (
-    <AppBar>
-      <Toolbar>
-        <LogoContainer>
-          <Logo />
-        </LogoContainer>
+    <>
 
-        <Typography color='inherit'>
-          Olá {userInfo.user.displayName.split(' ')[0]}
-        </Typography>
+      <AppBar>
+        <Toolbar>
+          <LogoContainer>
+            <Logo />
+          </LogoContainer>
 
-        <IconButton color='inherit' onClick={handleOpenMenu}>
-          <AccountCircle />
-        </IconButton>
+          <Typography color='inherit'>
+            Olá {userName}
+          </Typography>
 
-        <Menu open={!!anchorElement} onClose={handleClose} anchorEl={anchorElement}>
-          <MenuItem onClick={logout}>Sair</MenuItem>
-        </Menu>
-      </Toolbar>
-    </AppBar>
+          <IconButton color='inherit' onClick={handleOpenMenu}>
+            <AccountCircle />
+          </IconButton>
+
+          <Menu open={!!anchorElement} onClose={handleClose} anchorEl={anchorElement}>
+            <MenuItem onClick={logout}>Sair</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+
+      <Spacer />
+
+      <Content>
+        <Grid container justify='center'>
+          <Grid item>
+            <Typography variant='h3'>
+              O que vai ser {userName} ;-)
+            </Typography>
+          </Grid>
+        </Grid>
+      </Content>
+    </>
   )
 }
 
@@ -70,5 +89,22 @@ const Logo = styled(MainLogo)`
     stroke: #fff;
   }
 `
+
+const Content = styled.main`
+  padding: 20px;
+`
+
+const style = (theme) => ({
+  main: theme.mixins.toolbar
+})
+const SpaceWrapper = ({ classes }) => (
+  <div className={classes.main} />
+)
+
+const Spacer = withStyles(style)(SpaceWrapper)
+
+SpaceWrapper.propTypes = {
+  classes: t.object
+}
 
 export default Main
--- my-examples/m04/reactzzaria/sistema-pedidos/src/root.js
+++ my-examples/m04/reactzzaria/sistema-pedidos/src/root.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 import { BrowserRouter, Route } from 'react-router-dom'
-import { CssBaseline } from '@material-ui/core'
+import { CssBaseline, createMuiTheme, MuiThemeProvider } from '@material-ui/core'
 
 import App from './app'
 import AuthProvider from 'contexts/auth'
 
+const theme = createMuiTheme({})
+
 const Root = () => (
-  <AuthProvider>
-    <CssBaseline />
-    <BrowserRouter>
-      {/*
-        Colocando as informações de rota dentro do App
-        utilizando o componente Route
-      */}
-      <Route component={App} />
-    </BrowserRouter>
-  </AuthProvider>
+  <MuiThemeProvider theme={theme}>
+    <AuthProvider>
+      <CssBaseline />
+      <BrowserRouter>
+        {/*
+          Colocando as informações de rota dentro do App
+          utilizando o componente Route
+        */}
+        <Route component={App} />
+      </BrowserRouter>
+    </AuthProvider>
+  </MuiThemeProvider>
 )
 
 export default hot(module)(Root)
